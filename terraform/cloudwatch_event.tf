f44b345e6377bcee2901f8521f4bff5107cb81bd
josh <joshuadlillie@gmail.com>
2025-07-01 11:14:12 -0400

    adding cloudwatch event rule

---
+++ terraform/cloudwatch_event.tf
@@ -0,0 +1,19 @@
+resource "aws_cloudwatch_event_rule" "lambda_schedule" {
+  name                = "run-twitter-bot-schedule"
+  description         = "Run Twitter bot on a schedule"
+  schedule_expression = "cron(0 * * * ? *)"  # every hour
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.lambda_schedule.name
+  target_id = "twitter-bot-lambda"
+  arn       = aws_lambda_function.my_lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_schedule.arn
+}
